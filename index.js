13f6ee60ab7416deb32c50aba356580b26255375
NurahadSarker <nurahadrobi451@gmail.com>
2025-11-12 05:44:31 +0600

    complete crud operation function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -7,10 +8,77 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+//SocialSparkDbUser
+//MwQ0f0QNg1RYgTSf
+
+const uri = "mongodb+srv://SocialSparkDbUser:MwQ0f0QNg1RYgTSf@nurahad.0txpfm4.mongodb.net/?appName=Nurahad";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('SocialSpark web server is running')
 })
 
-app.listen(port, () =>{
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('socialSpark_db')
+        const eventsCollection = db.collection('events')
+
+        app.get('/events', async(req, res) =>{
+            const cursor = eventsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/events', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await eventsCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.post('/events', async(req, res) =>{
+            const newEvent = req.body
+            const result = await eventsCollection.insertOne(newEvent)
+            res.send(result)
+        })
+
+        app.patch('/events/:id', async(req, res) =>{
+            const id = req.params.id;
+            const updatedEvents = req.body;
+            const query = {_id: new ObjectId(id)}
+            const update = {
+                $set: updatedEvents
+            }
+            const result = await eventsCollection.updateOne(query, update)
+            res.send(result)
+        })
+
+        app.delete('/events/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await eventsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        
+    }
+}
+
+run().catch(console.dir)
+
+app.listen(port, () => {
     console.log(`SocialSpark web server is running port on: ${port}`)
 })
