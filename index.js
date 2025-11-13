1833a95a0c3c727d39fc98bfdbea954ff8f7a64d
NurahadSarker <nurahadrobi451@gmail.com>
2025-11-13 20:15:35 +0600

    added .env file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -8,10 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-//SocialSparkDbUser
-//MwQ0f0QNg1RYgTSf
 
-const uri = "mongodb+srv://SocialSparkDbUser:MwQ0f0QNg1RYgTSf@nurahad.0txpfm4.mongodb.net/?appName=Nurahad";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nurahad.0txpfm4.mongodb.net/?appName=Nurahad`;
 
 const client = new MongoClient(uri, {
     serverApi: {
